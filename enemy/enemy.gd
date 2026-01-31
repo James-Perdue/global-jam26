cfb1a9086b018f47af6d8fa92eeb97f3fbd0874d
BT100 Building <BT100@ac.sdmesa.net>
2026-01-31 11:54:52 -0800

    multiple emotions now availible

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -5,6 +5,7 @@ signal enemy_defeated
 @export var max_active_masks: int = 3
 
 var emotion_targets: Array[EmotionMessage] = []
+var emotion_count: int = 0
 var targeting_message_index: int = 0
 var targeting_emotion_index: int = 0
 
@@ -24,10 +25,11 @@ func _ready() -> void:
 
 func start_encounter() -> void:
 	show()
-	# Only pick one emotion target for now
+	# Only pick one message target for now
 	
 	emotion_targets = [EmotionDatabase.select_new_emotion_message()]
 	emotion_message_label.text = emotion_targets[0].message
+	emotion_count = len(emotion_targets[0].emotions)
 	_enable_masks()
 
 func _clear_masks() -> void:
@@ -49,9 +51,10 @@ func _enable_masks() -> void:
 		var mask: Mask = masks[mask_index]
 		var emotion: Enums.Emotion = Enums.Emotion.values().filter(func(e: Enums.Emotion): return not e in chosen_emotions).pick_random()
 		# TODO: handle multiple emotions in one message
+		
 		if(active_masks == 0):
 			# TODO: more emotions
-			emotion = emotion_targets[0].emotions[0]
+			emotion = emotion_targets[targeting_message_index].emotions[targeting_emotion_index]
 		chosen_emotions.append(emotion)
 		mask.set_emotion(emotion)
 		mask.hit.connect(_on_mask_hit.bind(mask))
@@ -67,13 +70,16 @@ func _on_mask_hit(mask: Mask) -> void:
 		SignalBus.wrong_mask.emit(mask)
 		return
 	print("Emotion Correct: ", mask.emotion)
-	targeting_message_index += 1
-	if targeting_message_index >= emotion_targets.size():
-		end_encounter()
-	else:
-		emotion_message_label.text = emotion_targets[targeting_message_index].message
-		_clear_masks()
-		_enable_masks()
+	targeting_emotion_index += 1
+	if targeting_emotion_index>=emotion_count:
+		targeting_message_index += 1
+		if targeting_message_index >= emotion_targets.size():
+			end_encounter()
+			return
+	
+	emotion_message_label.text = emotion_targets[targeting_message_index].message
+	_clear_masks()
+	_enable_masks()
 
 
 func end_encounter() -> void:
