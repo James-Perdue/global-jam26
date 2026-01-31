6bfd9fb22d1407d02aba490ee416cd7b8590325d
James-Perdue <perdue.james@gmail.com>
2026-01-31 15:12:48 -0800

    heal, interact, gun

---
+++ interactables/heal_item.gd
@@ -0,0 +1,17 @@
+extends Node3D
+class_name HealItem
+
+@export var heal_amount: int = 20
+
+@onready var mesh: MeshInstance3D = $InteractComponent/MeshInstance3D
+@onready var collision_shape: CollisionShape3D = $InteractComponent/CollisionShape3D
+@onready var interact_component: InteractComponent = $InteractComponent
+
+func _ready() -> void:
+	interact_component.interacted.connect(_on_interacted)
+
+func _on_interacted() -> void:
+	SignalBus.player_healed.emit(heal_amount)
+	collision_shape.disabled = true
+	mesh.visible = false
+	queue_free()
