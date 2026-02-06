835725ef15cca588895c559ffb9e487f97237d62
geoffery0 <geoffery@DESKTOP-9U7JLCN.localdomain>
2026-02-06 12:47:14 -0800

    increased wrong duration

--- ui/Text Effects/WrongEffect.gd
+++ ui/Text Effects/WrongEffect.gd
@@ -12,7 +12,7 @@ var bbcode := "ww"
 
 
 func _process_custom_fx(char_fx: CharFXTransform) :
-	var duration = char_fx.env.get("duration", 5)
+	var duration = char_fx.env.get("duration",7)
 	var t = (char_fx.elapsed_time*30) / duration
 	var a = clamp(t,0,1)
 	if t<1.3:
